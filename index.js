7b34da9482c3223c0e53fd5d5f24421de3d3e52b
Abdulrahman Yusuf <abdulrahmanyusuf125@gmail.com>
2021-04-04 17:54:00 +0100

    update: port & host

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require("./src/app");
 
-app.listen(process.env.PORT || 3002, (err, addr) => {
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, '0.0.0.0', (err, addr) => {
   if (err) throw err;
 });
